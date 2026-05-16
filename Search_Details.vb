a2e02d8374959c96136dc36b8adb56872087b542
Shubhi  pandey <shubhipandey8042005@gmail.com>
2026-05-16 11:03:22 +0530

    Delete Search_Details.vb

--- Search_Details.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Search_Details
-
-    Private Sub Search_Details_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'LoginInDataSet.Add_Delivery_Details' table. You can move, or remove it, as needed.
-        Me.Add_Delivery_DetailsTableAdapter.Fill(Me.LoginInDataSet.Add_Delivery_Details)
-
-    End Sub
-End Class
